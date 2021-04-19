6d886db2a461d56ef227c6777122309f0e09efb8
darrenmckenna <Darrenmckenna23@gmail.com>
2021-04-19 12:04:12 +0100

    Broken homepage into components

---
+++ src/components/directory-menu/directory-menu.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import MenuItem from '../menu-item/menu-item';
+import './directory-menu.styles.scss';
+
+const DirectoryMenu = () => {
+  const [sections, setSections] = useState([
+    {
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      id: 1,
+      linkUrl: 'shop/hats',
+    },
+    {
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      id: 2,
+      linkUrl: 'shop/jackets',
+    },
+    {
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      id: 3,
+      linkUrl: 'shop/sneakers',
+    },
+    {
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      size: 'large',
+      id: 4,
+      linkUrl: 'shop/womens',
+    },
+    {
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      size: 'large',
+      id: 5,
+      linkUrl: 'shop/mens',
+    },
+  ]);
+
+  return (
+    <div className="directory-menu">
+      {sections.map((section) => (
+        <MenuItem
+          title={section.title}
+          subtitle="SHOP NOW"
+          imageUrl={section.imageUrl}
+          size={section.size}
+          key={section.id}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default DirectoryMenu;
