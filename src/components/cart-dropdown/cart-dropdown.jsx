ce090f30b74cb5a7ae8f07f1ab65fe7d93c41622
darrenmckenna <Darrenmckenna23@gmail.com>
2021-04-27 15:35:30 +0100

    Create cart item component

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,11 +1,18 @@
 import './cart-dropdown.styles.scss';
 
 import CustomButton from '../form-components/custom-button/custom-button';
+import { useSelector } from 'react-redux';
+import CartItem from '../cart-item/cart-item';
 
 const CartDropdown = () => {
+
+  const cartItems = useSelector(state => state.cart.cartItems);
+
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map(item => <CartItem key={item.key} item={item}/>)}
+      </div>
       <CustomButton>Go TO CHECKOUT</CustomButton>
     </div>
   );
